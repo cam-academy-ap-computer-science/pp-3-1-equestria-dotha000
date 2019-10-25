dc115ce56fb9425bf5141aec52164945b80109f7
do_tha000 <do_tha000@CAM-LAB-23.bgsd.k12.wa.us>
2019-10-25 07:48:59 -0700

    New Save 1

--- src/Equestria.java
+++ src/Equestria.java
@@ -2,8 +2,20 @@
 public class Equestria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int x1 = 31;
+		int y1 = 35;
+		int x2 = 15;
+		int y2= 9;
+		double realDist = distance(x1, y1, x2, y2);
+		System.out.println("Distance from (" + x1 + "," + x2 + ") to (" + y1 + "," + y2 + ") = " + realDist);
+	}
+	public static double distance(int a1, int a2, int b1, int b2) {
+		double howFar = a2 - a1;
+		howFar = Math.pow(howFar, 2);
+		double howFar2 = b2 - b1;
+		howFar2 = Math.pow(howFar2, 2);
+		double dist = Math.sqrt(howFar + howFar2);
+		return dist;
 	}
 
 }
